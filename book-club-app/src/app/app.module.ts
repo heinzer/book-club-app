b6c1b231fa39c615038f8a74a2d8c544765ed15b
Lyzzi Brooks <lyzzibrooks@Lyzzis-MacBook-Air.local>
2022-02-03 17:59:50 -0600

    add register page

--- book-club-app/src/app/app.module.ts
+++ book-club-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 // import { AuthService } from './services/auth.service';
 
@@ -15,6 +16,7 @@ import { HomeComponent } from './pages/home/home.component';
     AppComponent,
     LoginComponent,
     HomeComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
--- book-club-app/src/app/pages/login/login.component.ts
+++ book-club-app/src/app/pages/login/login.component.ts
@@ -33,7 +33,7 @@ export class LoginComponent implements OnInit {
   }
 
   register() {
-
+    this.router.navigate(['/register']);
   }
 
 }

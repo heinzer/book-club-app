2fffdc0dcbe3dfdda2c053bf6e2da84ee8f04be0
Erin Heinz <erinheinz@cleo.com>
2022-03-26 21:31:51 -0500

    Cleaning up some styling and making a reusable hover container

---
+++ src/app/shared/containers/solid-container-with-hover/solid-container-with-hover.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-solid-container-with-hover',
+  template: '<ng-content></ng-content>',
+  styleUrls: ['./solid-container-with-hover.component.scss']
+})
+export class SolidContainerWithHoverComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from '../app.component';
 import { PasswordInputComponent } from './password-input/password-input.component';
 import { FancyDeadlinePipe, DeadlinePipe, DeadlinePercentagePipe } from './deadlines.pipe';
-import {DottedContainerComponent} from './dotted-container/dotted-container.component';
+import {DottedContainerComponent} from './containers/dotted-container/dotted-container.component';
 import {BasePageComponent} from './base-page/base-page.component';
-import {SolidContainerComponent} from './solid-container/solid-container.component';
+import {SolidContainerComponent} from './containers/solid-container/solid-container.component';
+import {SolidContainerWithHoverComponent} from './containers/solid-container-with-hover/solid-container-with-hover.component';
 
 export const components = [
   BasePageComponent,
   DottedContainerComponent,
   PasswordInputComponent,
-  SolidContainerComponent
+  SolidContainerComponent,
+  SolidContainerWithHoverComponent
 ];
 
 export const pipes = [
